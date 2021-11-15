9f68faf32c9acd8d70460b31f43bedf082681bb2
Elzenaro <nam2626@gmail.com>
2021-11-15 20:19:56 +0900

    type 예제

--- Section03/02_type.py
+++ Section03/02_type.py
@@ -1,9 +1,11 @@
+#type 현재 변수나 값이 어떤 데이터 타입인지 알려주는 함수
 num = 10
 pi = 3.1415
 flag = True
 name = "홍길동"
 list1 = []
 print(type(num))
+print(type(3))
 print(type(pi))
 print(type(flag))
 print(type(name))
