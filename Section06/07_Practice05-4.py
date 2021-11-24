119d613417311f6e0c125bcbf04babc63f700bc5
Elzenaro <nam2626@gmail.com>
2021-11-24 21:43:22 +0900

    if문

--- Section06/07_Practice05-4.py
+++ Section06/07_Practice05-4.py
@@ -1,5 +1,5 @@
 car_no = input('차량번호를 입력하세요 >>>')
-if car_no[-1] % 2 == 0:
+if int(car_no[-1]) % 2 == 0:
     print(f'차량번호 {car_no}는 오늘 운행이 가능합니다.')
 else:
     print(f'차량번호 {car_no}는 오늘 운행이 불가능합니다.')
