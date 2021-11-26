301fa8fa749d17c1789c43d092b8858c99da2164
Elzenaro <nam2626@gmail.com>
2021-11-26 21:25:03 +0900

    반복문

---
+++ Section07/11_for_ex_2.py
@@ -0,0 +1,12 @@
+for i in range(5): #range(n) 0~n-1까지 숫자를 하나씩 뽑음
+    print(i)
+print('------------')
+
+for i in range(1,10):#1부터 10전까지 숫자를 하나씩 뽑음
+    print(i)
+print('------------')
+#range(시작값,종료값+1,증감값)
+for i in range(1,10,2):#1부터 10전까지 숫자를 하나씩 뽑음 2씩 증가
+    print(i)
+print('------------')
+
