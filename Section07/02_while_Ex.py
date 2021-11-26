4edd2516c6e7b9fa9e7f15a08614f4cce91eb4d9
Elzenaro <nam2626@gmail.com>
2021-11-26 19:24:52 +0900

    반복문

---
+++ Section07/02_while_Ex.py
@@ -0,0 +1,8 @@
+my_list = []
+n = int(input('정수를 입력하세요(종료는 0 입니다)'))
+
+while n != 0:
+    my_list.append(n)
+    n = int(input('정수를 입력하세요(종료는 0 입니다)'))
+
+print(my_list)
---
+++ Section07/03_while_in_while.py
@@ -0,0 +1,7 @@
+dan = 2
+while dan <= 9:
+    n = 1
+    while n <= 9:
+        print('{}*{}={}'.format(dan,n,dan*n))
+        n+=1
+    dan += 1
