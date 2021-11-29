6711d067f4c6bc0d31817e89dea838852f61878a
Elzenaro <nam2626@gmail.com>
2021-11-29 20:01:50 +0900

    break

---
+++ Section08/02_break_2.py
@@ -0,0 +1,7 @@
+while True:
+    city = input('대한민국의 수도는 어디인가요? >>> ')
+    if city == '서울' or city == 'seoul' or city == 'SEOUL':
+        print('정답입니다.')
+        break
+    else:
+        print('오답입니다. 다시 시도 하세요.')
