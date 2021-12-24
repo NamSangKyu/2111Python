69d19f09b2692031335dd41e92893c336ee19045
Elzenaro <nam2626@gmail.com>
2021-12-24 19:36:25 +0900

    클래스

---
+++ Section16/08_class_eq.py
@@ -0,0 +1,22 @@
+class Number:
+    def __init__(self,n1,n2):
+        self.n1 = n1
+        self.n2 = n2
+
+    def sum(self):
+        return self.n1 + self.n2
+
+    #인스턴스 끼리 비교할때 사용되는 메서드
+    # ==
+    def __eq__(self, other):
+        print('__eq__ 실행')
+        return self.n1 == other.n1 and self.n2==other.n2
+
+n1 = Number(10,20)
+n2 = Number(10,20)
+n3 = n1
+print(n1.sum())
+print(n2.sum())
+print(n1,n2,n3)
+print(n1 == n2) #일반적으로는 메모리 주소값으로 비교
+print(n1 == n3)
