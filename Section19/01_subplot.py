52e869aaab54c0289ad06991195f7b3c1464f0db
Elzenaro <nam2626@gmail.com>
2022-01-03 19:19:28 +0900

    차트

---
+++ Section19/01_subplot.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+fiqure = plt.figure()
+#1행 2열  1번째 차트
+axes1 = fiqure.add_subplot(1,2,1)
+#1행 2열  2번째 차트
+axes2 = fiqure.add_subplot(1,2,2)
+#2행 2열  1번째 차트
+axes3 = fiqure.add_subplot(2,2,1)
+#2행 2열  2번째 차트
+axes4 = fiqure.add_subplot(2,2,2)
+
+
+plt.show()
